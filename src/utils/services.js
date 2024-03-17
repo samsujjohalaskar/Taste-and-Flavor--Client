c0174f80559e53ad3d748c79effa9ab7407123bd
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-03-17 17:24:54 +0530

    BASE_URL restored

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://taste-and-flavor-server.onrender.com";
-// export const BASE_URL = "https://taste-and-flavor-server.vercel.app";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://taste-and-flavor-server.vercel.app";
+// export const BASE_URL = "http://localhost:5000";
