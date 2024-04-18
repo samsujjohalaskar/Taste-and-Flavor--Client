cefffb24217679f5602f39981ef7403af12c4780
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-04-18 19:59:33 +0530

    strict mode removed

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { CityProvider } from "./CityContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <CityProvider>
       <App />
     </CityProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
